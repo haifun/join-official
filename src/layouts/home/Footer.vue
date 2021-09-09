9f7399019d814ad251bf167669f40cad47bc084d
haifun <haifuncn@gmail.com>
2021-09-09 16:06:17 +0800

    first init

---
+++ src/layouts/home/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-footer
+    id="home-footer"
+    color="black"
+    dark
+    min-height="72"
+  >
+    <v-container>
+      <v-row>
+        <v-col
+          class="text-center"
+          cols="24"
+        >
+          Copyright &copy; {{ new Date().getFullYear() }} Hangzhou Zhongyin Technology Co., Ltd.
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    name: 'HomeFooter',
+
+    data: () => ({
+      social: [
+      ],
+    }),
+  }
+</script>
+
+<style lang="sass">
+  #home-footer a
+    text-decoration: none
+</style>
---
+++ src/views/sections/ThemeFeatures.vue
@@ -0,0 +1,45 @@
+<template>
+  <base-section id="theme-features">
+    <base-section-heading title="PRODUCTS">
+      GXChain is a fundamental blockchain for the global data economy, designed to build a trusted data internet of value.
+Benefiting from DPoS based Graphene underlying architecture, GXChain possesses functions including G-ID, GVM, BaaS and Blockcity, which are convenient for application development. GXChain based DApp-Blockcity has more than two million verified users and provides abundant data for other DApps and strategic partners. GXChain launched decentralized data marketplace as well which is serving hundreds of Chinese enterprises. GXChain team independently developed its main net and launched it in June 2017. Based on decentralization, cryptography, and smart token design, GXChain provides a leading solution for the data economy by developing multiple trusted data modules. Data uploading, storage, computation, and exchange has been gradually realized with many commercialized applications.
+    </base-section-heading>
+  </base-section>
+</template>
+
+<script>
+  export default {
+    name: 'SectionThemeFeatures',
+
+    data: () => ({
+      features: [
+        {
+          title: 'Pixel Perfect Design',
+          icon: 'mdi-fountain-pen-tip',
+        },
+        {
+          title: 'Retina Ready',
+          icon: 'mdi-cellphone',
+        },
+        {
+          color: 'primary',
+          dark: true,
+          title: 'Easily Customizable',
+          icon: 'mdi-pencil-box-outline',
+        },
+        {
+          title: 'Image Parallax',
+          icon: 'mdi-image-size-select-actual',
+        },
+        {
+          title: 'Seo Optimized',
+          icon: 'mdi-ice-pop',
+        },
+        {
+          title: '24/7 Support',
+          icon: 'mdi-help-circle-outline',
+        },
+      ],
+    }),
+  }
+</script>
